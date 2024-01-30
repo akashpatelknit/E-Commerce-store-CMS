ca4fa6424896dd8aeddd7a0d1edc86b763a66ddb
akashpatelknit <cotsec14@gmail.com>
2024-01-31 04:12:01 +0530

    product card propes fixed

--- actions/get-billboard.tsx
+++ actions/get-billboard.tsx
@@ -1,8 +1,8 @@
-import { BillBoard } from '@/types';
+import { Billboard } from '@/types';
 
 const URL = `${process.env.NEXT_PUBLIC_API_URL}/billboards`;
 
-const getBillboard = async (id: string): Promise<BillBoard[]> => {
+const getBillboard = async (id: string): Promise<Billboard[]> => {
 	const res = await fetch(`${URL}/${id}`);
 	return res.json();
 };
