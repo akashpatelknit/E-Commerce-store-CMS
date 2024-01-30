ece522f0bbd3a61b9be64eac778b3e15e608c42e
akashpatelknit <cotsec14@gmail.com>
2024-01-31 04:27:53 +0530

    product card propes fixed

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,23 +1,24 @@
 import getBillboard from '@/actions/get-billboard';
 import getProducts from '@/actions/get-products';
-import BillBoard from '@/components/billboard';
 import ProductList from '@/components/product-list';
+import Billboard from '@/components/ui/billboard';
 import Container from '@/components/ui/container';
+
 export const revalidate = 0;
 
 const HomePage = async () => {
 	const products = await getProducts({ isFeatured: true });
 	const billboard = await getBillboard(
-		'497b7ea3-d7df-4f32-b720-0edba44b8896'
+		'0e12e5cf-29ab-4529-b8d5-c5371dae1f7b'
 	);
 
 	return (
 		<Container>
-			<div className=" space-y-10 pb-10">
-				<BillBoard data={billboard} />
-			<div className=" flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
-				<ProductList title="Featured Products" items={products} />
-			</div>
+			<div className="space-y-10 pb-10">
+				<Billboard data={billboard} />
+				<div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+					<ProductList title="Featured Products" items={products} />
+				</div>
 			</div>
 		</Container>
 	);
